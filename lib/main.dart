0a323075459e79e79e5735dc7b13314e75ba978f
Dhrumil <dhrumil@dhrumils-mbp.home>
2024-01-31 12:53:43 -0500

    expand all buttons with diffrent colors and musics

--- lib/main.dart
+++ lib/main.dart
@@ -4,62 +4,39 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void countnumber(int number){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$number.wav'));
+  }
+  Expanded expandbutton({required Color colors,required int number}){
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          countnumber(number);
+
+        },
+        child:Text('',),
+        style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(colors),),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            children: [
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note1.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.pink ),),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note2.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.green ),),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note3.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.yellow ),),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note4.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.blue ),),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note5.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.orange ),),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note6.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.purple ),),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note7.wav'));
-                },
-                child: Text("Click me",style: TextStyle(backgroundColor:Colors.red ),),
-              ),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:<Widget> [
+              expandbutton(colors: Colors.red, number: 1),
+              expandbutton(colors: Colors.purple, number: 2),
+              expandbutton(colors: Colors.orange, number: 3),
+              expandbutton(colors: Colors.pink, number: 4),
+              expandbutton(colors: Colors.yellow, number: 5),
+              expandbutton(colors: Colors.green, number: 6),
+              expandbutton(colors: Colors.blue, number: 7),
+
 
             ],
           ),
